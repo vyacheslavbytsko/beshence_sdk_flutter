58df6ebd2a91e77dd87f4b8b7c4d7fabc613adbf
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2026-07-04 15:59:45 +0300

    remove comments

--- lib/src/models/event.dart
+++ lib/src/models/event.dart
@@ -1,34 +1,3 @@
-import 'chain.dart';
-
-/*typedef BeshenceEventFromJson = BeshenceEvent Function(Map<String, dynamic>);
-typedef BeshenceEventToJson = Map<String, dynamic> Function(BeshenceEvent);
-
-class BeshenceEventRegistry {
-  final _events = <String, Type>{};
-
-  void register<T extends BeshenceEvent<T>>(String name) {
-    _events[name] = T;
-  }
-
-  Map<String, Type> get events => _events;
-}
-
-abstract class BeshenceEvent<T extends BeshenceEvent<T>> {
-  late final String _id;
-  late final BeshenceChain _chain;
-  final String name;
-  late final bool _applied;
-
-  BeshenceEvent({required this.name});
-
-  static T fromJson<T extends BeshenceEvent<T>>(Map<String, dynamic> json);
-  static Map<String, dynamic> toJson();
-
-  String get id => _id;
-  BeshenceChain get chain => _chain;
-  bool get applied => _applied;
-}*/
-
 abstract class BeshenceEvent {
   late final String _id;
 
